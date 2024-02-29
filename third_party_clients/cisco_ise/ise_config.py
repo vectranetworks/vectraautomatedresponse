a86ab159d1249c26a73ebdbd7c79188e9b358269
Brandon Wyatt <bwyatt@vectra.ai>
2024-02-29 13:52:53 -0500

    Utilizing Cisco's Devnet corrected issues with the Cisco ISE TCP. 1) CSRF decorator caused issues even when not used. 2) CSRF stored the CSRF token incorrectly in the dictionary. 3) The monitor lookup call required XML application headers instead of JSON. Headers added and call corrected.

--- third_party_clients/cisco_ise/ise_config.py
+++ third_party_clients/cisco_ise/ise_config.py
@@ -1,6 +1,6 @@
-ISE_APPLIANCE_IP = 'https://<IP_ADDRESS>'
-ISE_USERNAME = 'username'
-ISE_PASSWORD = 'password'
+ISE_APPLIANCE_IP = 'https://<appliance ip>'
+ISE_USERNAME = '<username>'
+ISE_PASSWORD = '<password>'
 CHECK_SSL = False
 # We need first to put the endpoint in a temporary policy to make the port bounce
 PORTBOUNCE_POLICY = 'PortBounce'
