5c5a08f13c105d467c11f0662da7b84b2c39accf
bwyatt <bwyatt@vectra.ai>
2025-04-25 10:11:39 -0400

    Fixed bug in validating tokens with --loop. Fixed bug with --no_store_keys

--- common.py
+++ common.py
@@ -11,12 +11,14 @@ def _get_password(system, key, **kwargs):
         return env_value
     store_keys = kwargs["modify"][0]
     update_keys = kwargs["modify"][1]
-    password = keyring.get_password(system, key)
-    if update_keys:
+    if not store_keys:
         password = getpass.getpass(f"Enter the {system} {key}: ")
-    elif password is None or password == "":
-        password = getpass.getpass(f"Enter the {system} {key}: ")
-    if store_keys:
+    else:
+        password = keyring.get_password(system, key)
+        if update_keys:
+            password = getpass.getpass(f"Enter the {system} {key}: ")
+        elif password is None or password == "":
+            password = getpass.getpass(f"Enter the {system} {key}: ")
         if password is not None:
             try:
                 keyring.set_password(system, key, password)
